8917350f0517584bf2c4b12d027b3da9596d9041
Erick Rodrigues <erick_brasil22@hotmail.com>
2023-11-19 23:34:29 -0300

    criação do GlobalExceptionHandler

--- src/main/java/br/com/empresaaleatoria/cadastrocia/config/GlobalExceptionHandler.java
+++ src/main/java/br/com/empresaaleatoria/cadastrocia/config/GlobalExceptionHandler.java
@@ -1,22 +1,23 @@
 package br.com.empresaaleatoria.cadastrocia.config;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import jakarta.persistence.EntityNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
 @ControllerAdvice
-public class CustomResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, Object>> handleValidationException(MethodArgumentNotValidException ex, WebRequest request) {
@@ -40,6 +41,11 @@ public class CustomResponseEntityExceptionHandler {
         response.put("atributos", fieldErrors);
 
         return ResponseEntity.status(status).body(response);
+    }
+    
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
 
