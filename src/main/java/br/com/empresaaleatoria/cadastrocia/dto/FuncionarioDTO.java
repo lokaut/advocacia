5b1558832465aba03f870e74734cef434606e68d
Erick Rodrigues <erick_brasil22@hotmail.com>
2023-11-19 23:36:01 -0300

    validação no dto

--- src/main/java/br/com/empresaaleatoria/cadastrocia/dto/FuncionarioDTO.java
+++ src/main/java/br/com/empresaaleatoria/cadastrocia/dto/FuncionarioDTO.java
@@ -3,12 +3,20 @@ package br.com.empresaaleatoria.cadastrocia.dto;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.format.annotation.NumberFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -21,38 +29,56 @@ public class FuncionarioDTO {
 	@NotBlank(message = "O campo 'nome' não pode estar em branco.")
     private String nome;
 
-    private String cpf;
+	@CPF(message = "CPF inválido")
+	private String cpf;
 
     private String logradouro;
+    
+    @Null(message = "ativo precisa ser nulo")
+    private Boolean ativo;
 
+    @NumberFormat
     private String numero;
 
     private String bairro;
 
+    @Pattern(regexp = "^[\\p{L} \\s]*$", message = "O campo deve conter apenas letras, espaços e acentos.")
     private String municipio;
 
+    @Size(max = 2)
     private String uf;
 
     private String codigoIbge;
 
+    @Pattern(regexp = "[0-9]{8}$", message = "O campo  cep deve conter apenas numeros de 8 digitos.")
     private String cep;
 
     private String complemento;
 
+    @Email(message = "email inválido")
     private String email;
 
+    @Pattern(regexp = "^([1-9]{2})$", message = "O ddd deve conter apenas numeros")
     private String ddd;
 
+    @Pattern(regexp = "^9?[0-9]{8}$", message = "O telefone deve conter apenas numeros")
     private String telefone;
 
+    @Null(message = "salario precisa ser nulo")
     private BigDecimal salario;
 
-    private String descricao;
+	@Pattern(regexp = "^(?i)(estagiario|junior|pleno|senior)$", message = "A descrição do cargo deve ser 'estagiario', 'junior', 'pleno' ou 'senior'.")
+	@NotBlank(message = "O campo 'descricao' não pode estar em branco.")
+    private String cargoNome;
+	
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private String dataAdmissao;
 
-    private LocalDate dataAdmissao;
-
-    private LocalDate dataNascimento;
-
-    private LocalDate dataDesligamento;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    @NotNull(message = "dataNascimento não pode ser nulo")
+    private String dataNascimento;
+    
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private String dataDesligamento;
 
 }
